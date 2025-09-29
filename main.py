472576b6f585118951daade67f6602ced57022cc
harish151 <hr2946352@gmail.com>
2025-09-29 21:51:33 +0530

    commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/helloworld')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/add')
+def add_numbers():
+    # Get numbers from query parameters: /add?a=5&b=3
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a is None or b is None:
+        return 'Please provide both "a" and "b" query parameters.', 400
+    result = a + b
+    return f'The sum of {a} and {b} is {result}.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
